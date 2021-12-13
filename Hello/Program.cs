e0bf609db66953838197eb416a13a7a22b1c3611
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-13 15:40:55 +0530

    first

---
+++ Hello/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is important for debugging. Do not remove it.
+            /*
+            This is a multi-line comment.
+            Spanning in multiple lines.
+             */ 
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Shubham");
+            Console.WriteLine("I love C#");
+            Console.ReadLine();
+        }
+    }
+}
